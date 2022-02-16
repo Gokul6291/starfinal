5b0d0763eda1b84151aea425a75fe89338d8d657
Gokul6291 <60608177+Gokul6291@users.noreply.github.com>
2022-02-16 16:04:49 +0530

    Update airflow_final_code.py

--- airflowdag/airflow_final_code.py
+++ airflowdag/airflow_final_code.py
@@ -82,7 +82,7 @@ def post_validation(**kwargs):
     
     
     # Availability check
-    
+   
     
     if df_stagingzone[df_stagingzone.columns[0]].count() != 0: # df.columns[0] = 1st column
         # Count check
